7906b73bd288af7ef9ff251f7549da7d1383aec2
mail2418 <muhammadismail2418@gmail.com>
2024-02-02 16:16:06 +0700

    feat: add new code

---
+++ controllers/mainController.go
@@ -0,0 +1,36 @@
+package controllers
+
+// import "github.com/gin-gonic/gin"
+
+// Sign up
+// func SignUp() gin.HandlerFunc{
+
+// }
+// // Hash Password
+// func HashPassword(password string) string {
+
+// }
+// // Verify Password
+// func VerifyPassword(userPassword string, givenPassword string) (bool,string) {
+
+// }
+
+// // Login
+// func Login() gin.HandlerFunc{
+
+// }
+
+// // Product View Admin
+// func ProductViewerAdmin() gin.HandlerFunc{
+
+// }
+
+// // Search Product
+// func SearchProduct() gin.HandlerFunc{
+
+// }
+
+// // Search Product by Query
+// func SearchProductByQuery() gin.HandlerFunc{
+
+// }
